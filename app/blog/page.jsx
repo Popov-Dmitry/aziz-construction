8dbdfb8e29a2e80e003495785ec0cb03a28b7552
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-04-03 18:36:27 +0900

    Added Blog page

---
+++ app/blog/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./blog.module.css";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Archives</h1>
+    </div>
+  );
+};
+
+export default Blog;
