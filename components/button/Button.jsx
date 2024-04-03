88762f990aed81dba2c5ab8e20e7fb3f2abfe826
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-04-03 13:36:27 +0900

    Added Header

---
+++ components/button/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./button.module.css";
+import { joinClassNames } from "@/utils/join-class-names";
+import Link from "next/link";
+
+
+const Button = ({ text, variant = "filled", color = "red", type, onClick, className, href }) => {
+  if (href) {
+    return (
+      <Link className={joinClassNames(styles.button, styles[variant], styles[color], className)} href={href}>
+        {text}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      type={type}
+      className={joinClassNames(styles.button, styles[variant], styles[color], className)}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
---
+++ components/header/Header.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
+import { joinClassNames } from "@/utils/join-class-names";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const path = usePathname();
+  console.log(path);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.wrapper}>
+          <Link href={"/"}>
+            <Image src={"/logo.png"} alt={"Aziz Construction"} width={112} height={91} />
+          </Link>
+          <div className={styles.elements}>
+            <Link
+              className={joinClassNames(styles.element, path === "/" ? styles.active : undefined)}
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={joinClassNames(styles.element, path === "/about-us" ? styles.active : undefined)}
+              href="/about-us"
+            >
+              About Us
+            </Link>
+            <Link
+              className={joinClassNames(styles.element, path === "/our-services" ? styles.active : undefined)}
+              href="/our-services"
+            >
+              Our Services
+            </Link>
+            <Link
+              className={joinClassNames(styles.element, path === "/blog" ? styles.active : undefined)}
+              href="/blog"
+            >
+              Blog
+            </Link>
+            <Link
+              className={joinClassNames(styles.element, path.startsWith("/gallery") ? styles.active : undefined)}
+              href="/gallery"
+            >
+              Gallery
+            </Link>
+            <Link
+              className={joinClassNames(styles.element, path === "/contact-us" ? styles.active : undefined)}
+              href="/contact-us"
+            >
+              Contact Us
+            </Link>
+          </div>
+          <Button text="GET A QUOTE" variant="outlined" color="black" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
