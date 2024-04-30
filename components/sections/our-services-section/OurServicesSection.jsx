44aae9cec5d215ec21c84ea8bc7e0acc174344a3
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-05-01 00:31:06 +0900

    Connected Contentful with OurServicesSection

--- components/sections/our-services-section/OurServicesSection.jsx
+++ components/sections/our-services-section/OurServicesSection.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import styles from "./our-services-section.module.css";
-import { servicesData } from "@/data";
 import ServiceCard from "@/components/elements/service-card/ServiceCard";
+import { getServices } from "@/lib/api";
 
-const OurServicesSection = () => {
+const OurServicesSection = async () => {
+  const services = await getServices();
+
+  if (!services) {
+    return;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>Our Services</div>
       <div className={styles.subtitle}>What We Offer</div>
       <div className={styles.services}>
-        {servicesData.map((service) => (
+        {services.map((service) => (
           <ServiceCard
-            key={service.link}
-            src={service.image}
-            title={service.title}
-            description={service.description}
-            href={service.link}
+            key={service.slug}
+            src={service.cover?.url}
+            title={service.name}
+            description={service.shortDescription}
+            href={`/our-services/${service.slug}`}
           />
         ))}
       </div>
