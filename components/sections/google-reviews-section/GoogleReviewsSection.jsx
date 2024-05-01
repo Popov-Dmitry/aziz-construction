287fc95448691439975c74bac855f5e159577744
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-05-01 13:22:25 +0900

    Updated Google ratings

--- components/sections/google-reviews-section/GoogleReviewsSection.jsx
+++ components/sections/google-reviews-section/GoogleReviewsSection.jsx
@@ -3,6 +3,7 @@ import styles from "./googlereviews-section.module.css";
 import Reviews from "@/components/elements/reviews/reviews";
 import { reviewsData } from "@/data";
 import { joinClassNames } from "@/utils/join-class-names";
+import Link from "next/link";
 
 const GoogleReviewsSection = ({ className }) => {
   return (
@@ -12,9 +13,9 @@ const GoogleReviewsSection = ({ className }) => {
         <div className={styles.subtitle}>What Our Clients Says About Us</div>
       </div>
       <Reviews reviews={reviewsData} className={styles.reviews} />
-      <div className={styles.raitingSummary}>
-        <span className={styles.raitingSummaryBold}>Google</span> rating score: <span className={styles.raitingSummaryBold}>{(reviewsData.map((item) => item.raiting).reduce((prev, curr) => prev + curr) / reviewsData.length).toFixed(1)}</span> of 5, based on <span className={styles.raitingSummaryBold}>{reviewsData.length}</span> reviews
-      </div>
+      <Link href="https://maps.app.goo.gl/63AE7NpeyVnuYKk88" target="_blank" className={styles.raitingSummary}>
+        <span className={styles.raitingSummaryBold}>Google</span> rating score: <span className={styles.raitingSummaryBold}>5.0</span> of 5, based on <span className={styles.raitingSummaryBold}>40</span> reviews
+      </Link>
     </div>
   );
 };
