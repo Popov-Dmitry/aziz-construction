905bded55e92eafc29a3bea038fb17e410afd737
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-05-01 13:27:19 +0900

    Updated AreasServedSection

--- components/sections/areas-served-section/AreasServedSection.jsx
+++ components/sections/areas-served-section/AreasServedSection.jsx
@@ -10,22 +10,22 @@ const AreasServedSection = () => {
         <div className={styles.text}>
           Our geographical focus is servicing home owners within the San Francisco Peninsula Bay Area including:
         </div>
-        <div className={styles.areas}>
-          <li className={styles.area}>Atherton</li>
-          <li className={styles.area}>Burlingame</li>
-          <li className={styles.area}>Foster City</li>
-          <li className={styles.area}>Half Moon Bay</li>
-          <li className={styles.area}>Hillsborough</li>
-          <li className={styles.area}>Menlo Park</li>
-          <li className={styles.area}>Millbrae</li>
-          <li className={styles.area}>Palo Alto</li>
-          <li className={styles.area}>Redwood City</li>
-          <li className={styles.area}>San Carlos</li>
-          <li className={styles.area}>San Mateo</li>
-        </div>
         <div className={styles.row}>
           <Image src="/clock.svg" alt="" width={18} height={18} />
           <div className={styles.boldText}>8:00 AM - 8:00 PM</div>
+        </div>
+        <div className={styles.areas}>
+          <li>Atherton</li>
+          <li>Burlingame</li>
+          <li>Foster City</li>
+          <li>Half Moon Bay</li>
+          <li>Hillsborough</li>
+          <li>Menlo Park</li>
+          <li>Millbrae</li>
+          <li>Palo Alto</li>
+          <li>Redwood City</li>
+          <li>San Carlos</li>
+          <li>San Mateo</li>
         </div>
       </div>
       <div>
