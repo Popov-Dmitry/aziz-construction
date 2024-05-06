e89ec9e9266358f8d36ce00c4483aa9abd0202f1
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-05-06 10:48:29 +0900

    Fixed json ld validation errors

--- components/sections/youtube-reviews-section/YoutubeReviewsSection.jsx
+++ components/sections/youtube-reviews-section/YoutubeReviewsSection.jsx
@@ -9,8 +9,8 @@ const jsonLd1 = {
   "description": "\"Exciting news for our valued clients! We've just posted a comprehensive video on YouTube showcasing our latest whole-house remodeling project. Dive in to witness the transformation and gather inspiration for your own home. Don't forget to subscribe and share!\"",
   "uploadDate": "2023-09-25",
   "contentUrl": "https://youtu.be/uop_Ps-PBlA",
-  "embedUrl" : <iframe width="570" height="321" src="https://www.youtube.com/embed/uop_Ps-PBlA" title="Full House Renovation Testimonial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
-  "thumbnailUrl" : ""
+  "embedUrl": "https://www.youtube.com/embed/uop_Ps-PBlA",
+  "thumbnailUrl": ""
 };
 
 const jsonLd2 = {
@@ -20,8 +20,8 @@ const jsonLd2 = {
   "description": "Welcome to our latest kitchen remodel showcase in San Jose, presented by Aziz Construction. Our team's expertise shines in creating custom spaces tailored to our clients' visions. Dive into this video to witness our detailed transformation process. If you're in San Jose  and considering a renovation, trust in our unmatched professionalism and quality. Contact Aziz Construction for a consultation today. For exclusive insights and expert transformations, subscribe to our channel.",
   "uploadDate": "2023-09-25",
   "contentUrl": "https://youtu.be/vKwVM8xMsW8",
-  "embedUrl" : <iframe width="570" height="321" src="https://www.youtube.com/embed/vKwVM8xMsW8" title="Kitchen Remodel Testimonial in  San Jose" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
-  "thumbnailUrl" : ""
+  "embedUrl": "https://www.youtube.com/embed/vKwVM8xMsW8",
+  "thumbnailUrl": ""
 };
 
 const YoutubeReviewsSection = () => {
