e90698ed41c8acd45f699772b64447e8bcb2358d
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-05-16 19:00:28 +0900

    Updated video json-ld

--- components/sections/youtube-reviews-section/YoutubeReviewsSection.jsx
+++ components/sections/youtube-reviews-section/YoutubeReviewsSection.jsx
@@ -7,21 +7,23 @@ const jsonLd1 = {
   "@type": "VideoObject",
   "name": "Full House Renovation Testimonial",
   "description": "\"Exciting news for our valued clients! We've just posted a comprehensive video on YouTube showcasing our latest whole-house remodeling project. Dive in to witness the transformation and gather inspiration for your own home. Don't forget to subscribe and share!\"",
-  "uploadDate": "2023-09-25",
-  "contentUrl": "https://youtu.be/uop_Ps-PBlA",
+  "thumbnailUrl": "https://i.ytimg.com/vi/uop_Ps-PBlA/default.jpg",
+  "uploadDate": "2023-09-25T04:21:58Z",
+  "duration": "PT1M21S",
   "embedUrl": "https://www.youtube.com/embed/uop_Ps-PBlA",
-  "thumbnailUrl": ""
+  "interactionCount": "20"
 };
 
 const jsonLd2 = {
   "@context": "http://schema.org/",
   "@type": "VideoObject",
-  "name": "Kitchen Remodel Testimonial in San Jose",
-  "description": "Welcome to our latest kitchen remodel showcase in San Jose, presented by Aziz Construction. Our team's expertise shines in creating custom spaces tailored to our clients' visions. Dive into this video to witness our detailed transformation process. If you're in San Jose  and considering a renovation, trust in our unmatched professionalism and quality. Contact Aziz Construction for a consultation today. For exclusive insights and expert transformations, subscribe to our channel.",
-  "uploadDate": "2023-09-25",
-  "contentUrl": "https://youtu.be/vKwVM8xMsW8",
+  "name": "Kitchen Remodel Testimonial in  San Jose",
+  "description": "Welcome to our latest kitchen remodel showcase in San Jose, presented by Aziz Construction. Our team's expertise shines in creating custom spaces tailored to our clients' visions. Dive into this video to witness our detailed transformation process. If you're in San Jose  and considering a renovation, trust in our unmatched professionalism and quality. Contact Aziz Construction for a consultation today.  For exclusive insights and expert transformations, subscribe to our channel.",
+  "thumbnailUrl": "https://i.ytimg.com/vi/vKwVM8xMsW8/default.jpg",
+  "uploadDate": "2023-09-25T04:37:50Z",
+  "duration": "PT1M44S",
   "embedUrl": "https://www.youtube.com/embed/vKwVM8xMsW8",
-  "thumbnailUrl": ""
+  "interactionCount": "22"
 };
 
 const YoutubeReviewsSection = () => {
